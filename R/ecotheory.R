f2bfd601100933bee3fca964446c08b166010c26
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2014-11-12 10:08:18 -0800

    added just a little bit to package documentation

--- R/ecotheory.R
+++ R/ecotheory.R
@@ -1,5 +1,9 @@
 #' ecotheory
 #' 
+#' This is a compantion package to Vellend 201X. This package contains only 
+#' vignettes, which present detailed descriptions of code used in the book.
+#' These vignettes reproduce most figures from the text.
+#' 
 #' @name ecotheory
 #' @docType package
 NULL
