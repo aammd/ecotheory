47fd90a4078b0ef1f064244f7b4c3a2315bd5c9a
mvellend <mvellend@gmail.com>
2015-02-11 15:50:21 +1000

    Box2 read for converting to Rmd.

---
+++ MV_readme.txt
@@ -0,0 +1,10 @@
+Statuses of files:
+1. MODIFIED (changes have been made)
+2. STAGED (box clicked beside file name in R Studio)
+3. COMMITTED (declaration that these are github worthy)
+
+To get changes into github:
+1. PULL
+2. CLICK BOXES NEXT TO FILES (stage)
+3. COMMIT
+4. PUSH
--- originalR/Vellend_Box2(Figs1&2&3&6).R
+++ originalR/Vellend_Box2(Figs1&2&3&6).R
@@ -1,34 +1,43 @@
 # R code to produce Figures 4.1, 4.2, 4.3 and 4.6
-# Dynamics of 2 species under constand and/or frequency-dependent selection
-# Note that some parameters come outside first simulation loop; others are inside
-# For Fig. 1: num.sims = 20, num.years = 50, fit.ratio.avg = 1, freq.dep = 0, init.1 = 0.5*J, J varies among panels
+# Dynamics of 2 species under constant and/or frequency-dependent selection
+# Note that some parameters are outside the simulation loop; others are inside (defined anew for each simulation)
 
+# For Fig. 4.1 (neutral model): num.sims = 20, num.years = 50, fit.ratio.avg = 1, freq.dep = 0, init.1 = 0.5*J, J varies among panels
+# For Fig. 4.2 (constant selection): as for Fig. 4.1 except fit.ratio.avg = 1.1
+# For Fig. 4.3 (negative frequency-dependent selection): as for Fig. 4.1 except fit.ratio.avg, freq.dep and J vary among panels
+# For Fig. 4.6 (positive frequency-dependent selection): as for Fig. 4.1 except freq.dep = 0.4, J = 100 and init.1 varies among panels
 
-num.sims <- 20 # number of independent simulations to run
+## specify the number of simulations, the number of years, and a matrix for output
+num.sims <- 20 
 num.years <- 50
 freq.1.mat <- matrix(nrow = num.sims, ncol = num.years)
 
+## start a loop for each of j independent simulations
 for (j in 1:num.sims) {
   
+  ## specify parameters and initial conditions
   J <- 100
-  init.1 <- 0.5*J
+  init.1 <- 0.5*J  
+  
   COM <- vector(length = J)
   COM[1:init.1] <- 1; COM[(init.1+1):J] <- 2
-  
   year <- 2
   
   fit.ratio.avg <- 1
   freq.dep <- 0
   
+  ## record data (frequency of species 1) for year 1
   freq.1.mat[j,1] <- sum(COM==1)/J
   
+  ## run simulation
   for (i in 1:(J*(num.years-1))) {
     
     freq.1 <- sum(COM==1)/J; freq.2 <- 1 - freq.1
     fit.ratio <- exp(freq.dep*(freq.1-0.5) + log(fit.ratio.avg))
     Pr.1 <- fit.ratio*freq.1/(fit.ratio*freq.1 + freq.2)
     COM[ceiling(J*runif(1))] <- sample(c(1,2), 1, prob=c(Pr.1,1-Pr.1))
-    
+
+    ## record data    
     if (i %% J == 0) {
       freq.1.mat[j,year] <- sum(COM==1)/J
       year <- year + 1
@@ -36,6 +45,7 @@ for (j in 1:num.sims) {
   }
 }
 
+## graph the results
 plot(1:num.years, freq.1.mat[1,], type="l", xlab="Time", 
      ylab="Frequency of species 1", ylim=c(0,1))
 for (i in 2:(num.sims-1)) {
